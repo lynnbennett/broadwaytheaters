2f0288d0064da8012f48d9a02b564d8e13b77fac
phyllis L Bennett <plb2100@outlook.com>
2018-05-01 23:55:28 +0000

    final project

---
+++ js/myScript.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+$("#quiz").click(function(){
+    alert("The Phantom of the Opera, with 7486 performances.");
+});
+});
+
+
+/*modal*/
+
+$('#myModal').on('hidden.bs.modal', function (e) {
+  
+})
+
+/**/
+function myFunction() {
+    var x = document.getElementById("sr-only").name;
+    document.getElementById("inlineFormInputName2").innerHTML = x;
+}
+
+.btn-default {
+      border-radius: 13;
